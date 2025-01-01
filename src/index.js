45f79a351adf9086a8823dd237649261d6c75782
sweets-code <sweetscrave20@gmail.com>
2025-01-01 21:44:06 +1100

    Added basic javascript and updated html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "lava cake",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
